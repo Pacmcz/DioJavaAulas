5a412387af895d8138ae3e099743dc31ad876baa
Lucas <dev.pac.mcz@gmail.com>
2024-09-25 15:53:47 -0300

    Teste1

---
+++ src/BootCampClaro/Main.java
@@ -0,0 +1,7 @@
+package BootCampClaro;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Seja bem vindo(a) a minha trilha de conhecimento do Bootcamp da Claro! Estou feliz em compartilhar esses arquivos com você");
+    }
+}
