09e56a3c12f5d2f77da57410a200dca598f2f790
Lucas <dev.pac.mcz@gmail.com>
2024-09-28 09:46:06 -0300

    Primeiro commit de sábado

---
+++ src/BootCampClaro/aulas/TryCatch.java
@@ -0,0 +1,29 @@
+package BootCampClaro.aulas;
+
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class TryCatch {
+    public static void main(String[] args) {
+        try {
+            Scanner entrada = new Scanner(System.in).useLocale(Locale.US);
+            System.out.println("Digite o seu nome: ");
+            String nome = entrada.nextLine();
+
+            System.out.println("Digiteu seu sobrenome: ");
+            String sobrenome = entrada.nextLine();
+
+            System.out.println("Digite a sua idade: ");
+            int idade = entrada.nextInt();
+
+            System.out.println("Digite o seu tamanho: ");
+            double tamanho = entrada.nextDouble();
+
+            System.out.println("Olá, meu nome é " + nome.toUpperCase() + " " + sobrenome.toUpperCase());
+
+        } catch (InputMismatchException e) {
+            System.out.println("Os campos idade e altura precisam ser numéricos");
+        }
+    }
+}
